9f071af56a4a3b1849fdf144c7114832303885de
Amaan Khatib <amaankhatib01@gmail.com>
2023-05-17 00:55:31 +0530

    first commit

---
+++ admin/aboutus.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+    <title>About</title>
+<link rel="stylesheet" href="../csss/aboutus.css" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Flamenco" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+
+</head>
+<body>
+    <header>
+      <nav>
+        <div class="row clearfix">
+            <ul class="main-nav" animate slideInDown>
+                <li><a href="../index.php">HOME</a></li>
+                <li><a href="aboutus.php">ABOUT</a></li>
+                <li><a href="contactus.php">CONTACT</a></li>
+                <li><a href="../login.php">ADMIN LOGIN</a></li>
+          </ul>
+        </div>
+      </nav>
+      <div class="main-content-header">
+          <h2 class="search">About Project</h2>
+             <div class="div2">
+             <h3>Student Result Management System</h2>
+             <p>The main objective of this project is to provide result to the student in a simple way.</p>
+             <p>The Student can get their result from this website by their roll numbers and class.</p>
+             <p>The admin login here is for the teacher of perticular class,where he/she can add marks ,upadate or can delete the marks.Here one can find all student data also.teacher can also able to see messages from students.</p>
+             <p> Student Result Management System is a technological opportunity for the school, college university and coaching centre institutions searching for a secure, simple and alternative solution to the conventional paper-based exam results  evaluation, reporting and distribution.</p>
+             </div>  
+          </div>
+    </header>
+    
+</body>
+</html>
